d61bf6742b503f7ae05a0d6e6d37c61f7d44a124
pavel11111111111111111111 <55051040+pavel11111111111111111111@users.noreply.github.com>
2020-03-18 18:00:46 +0300

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,8 +1,28 @@
 import boto3
 
+s3=boto3.client('s3')
 l=boto3.client('lambda')
 
+#l.add_permission(
+#  StatementId  = "AllowExecutionFromS3Bucket",
+#  Action        = "lambda:InvokeFunction",
+#  FunctionName = "trigtest",
+#  Principal     = "s3.amazonaws.com",
+#  SourceArn    = "arn:aws:s3:::forgit"
+#)
 
-l.publish_version(FunctionName='get_device_data')
 
-print(l)
+
+
+response = client.update_function_code(
+    FunctionName='string',
+    S3Bucket='string',
+    S3Key='string',
+
+)
+
+
+
+response3 = s3.put_bucket_notification_configuration(
+                            Bucket='forgit',
+                            NotificationConfiguration= {'LambdaFunctionConfigurations':[{'LambdaFunctionArn': 'arn:aws:lambda:us-east-2:301691100982:function:trigtest', 'Events': ['s3:ObjectCreated:*']}]})
